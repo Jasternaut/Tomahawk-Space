0726842f68ef19bb0fe8d43f3f90bf560f4355a7
Jasternaut <patrushev2006@list.ru>
2024-09-04 22:21:58 +0200

    Добавьте файлы проекта.

---
+++ Tomahawk Space/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tomahawk_Space
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
