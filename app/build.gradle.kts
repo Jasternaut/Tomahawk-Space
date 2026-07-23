e713d8ad5f59ce18c048fc59c269e4afda10a1ab
Jasternaut <patrushev2006@list.ru>
2026-07-23 16:02:02 +0200

    chore: add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.material)
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
